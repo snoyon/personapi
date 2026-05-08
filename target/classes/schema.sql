e1abe7b2d9aa9623305ef3225367f9641162e227
unknown <snoyon@hotmail.com>
2026-05-08 20:11:58 +0200

    Ignore Maven target directory

--- target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
-create table personne (
-    id bigint primary key,
-    nom varchar(100) not null,
-    prenom varchar(100) not null,
-    email varchar(255) not null,
-    telephone varchar(30) not null,
-    adresse_postale varchar(255) not null,
-    salaire_mensuel numeric(12, 2) not null
-);
